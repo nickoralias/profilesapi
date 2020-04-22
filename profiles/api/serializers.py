68445ea2700eda589d1ce29c558654ca9b44ecca
Nick Lofaro <5376991+nickoralias@users.noreply.github.com>
2020-04-21 23:07:30 -0700

    Use viewsets/router

--- profiles/api/serializers.py
+++ profiles/api/serializers.py
@@ -25,3 +25,4 @@ class ProfileStatusSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProfileStatus
+        fields = '__all__'
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,10 +1,38 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from profiles.models import Profile
-from profiles.api.serializers import ProfileSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
+from rest_framework import mixins
+from profiles.models import Profile, ProfileStatus
+from profiles.api.permissions import IsOwnProfileOrReadOnly, IsOwnerOrReadOnly
+from profiles.api.serializers import (ProfileAvatarSerializer,
+                                      ProfileSerializer,
+                                      ProfileStatusSerializer)
 
 
-class ProfileList(generics.ListAPIView):
+class AvatarUpdateView(generics.UpdateAPIView):
+    serializer_class = ProfileAvatarSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        profile_object = self.request.user.profile
+        return profile_object
+
+
+class ProfileViewSet(mixins.ListModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnProfileOrReadOnly]
+
+
+class ProfileStatusViewSet(ModelViewSet):
+    queryset = ProfileStatus.objects.all()
+    serializer_class = ProfileStatusSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        user_profile = self.request.user.profile
+        serializer.save(user_profile=user_profile)
